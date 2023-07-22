21710b6e9dcdc68447ca52db906d3c221b16f8b0
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-07-21 20:10:58 -0500

    .

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import axios from "axios"; // Import Axios
 import PrimeVue from "primevue/config";
 
 //theme
-import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/themes/lara-dark-indigo/theme.css";
 //core
 import "primevue/resources/primevue.min.css";
 //icons
@@ -15,6 +15,8 @@ import Button from "primevue/button";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import Toolbar from "primevue/toolbar";
+import Dialog from "primevue/dialog";
+import InputText from "primevue/inputtext";
 const app = createApp(App);
 app.use(PrimeVue);
 
@@ -23,5 +25,7 @@ app.component("Button", Button);
 app.component("DataTable", DataTable);
 app.component("Column", Column);
 app.component("Toolbar", Toolbar);
+app.component("Dialog", Dialog);
+app.component("InputText", InputText);
 
 app.mount("#app");
